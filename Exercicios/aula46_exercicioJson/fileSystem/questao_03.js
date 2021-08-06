8d7293290a014c659f27a056974374e1f235820a
Barbara <barbaraalessandramaas@gmail.com>
2021-08-06 11:52:44 -0300

    Exercícios Atualizados

---
+++ Exercicios/aula46_exercicioJson/fileSystem/questao_03.js
@@ -0,0 +1,3 @@
+// 3. Crie um script que leia o arquivo cpfs.txt e utilizando a biblioteca validator.js (https://github.com/validatorjs/validator.js/)
+// salve os cpfs válidos em um arquivo cpfsvalidos.txt e os cpfs inválidos em um arquivo cpfsinvalidos.txt.
+// */
