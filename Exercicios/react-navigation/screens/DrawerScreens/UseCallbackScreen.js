dca31f94ba70c13e49bf4adb628132aa1065d986
Barbara <barbaraalessandramaas@gmail.com>
2021-09-14 11:54:14 -0300

    Hooks

---
+++ Exercicios/react-navigation/screens/DrawerScreens/UseCallbackScreen.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { ScrollView, Text } from "react-native";
+
+export function UseCallbackScreen() {
+    return (
+        <ScrollView>
+            <Text>
+                Retorna um CALLBACK memoizado.
+
+                Recebe como argumentos um callback e um array. Segue o mesmo estilo do memo,
+                em que a cada renderização do componente, todo o código que está nele é executado 
+                novamente. As funções são re-declaradas, e uma nova referência (na memória) é 
+                alocada para cada função. O useCallback faz com que a função seja redefinida 
+                apenas quando necessário, assim mantendo a mesma referência.
+
+                O array de dependências é o que define se o callback será redefinido ou não. Caso 
+                haja uma mudança em alguma das dependências, será redefinido.
+
+                const callbackAtualizado = React.useCallback(() => {
+                    console.log('callbackAtualizado:', contador);
+                }, [contador]);
+  
+                const callbackNaoAtualizado = React.useCallback(() => {
+                    console.log('callbackNaoAtualizado:', contador);
+                }, []);
+            </Text>  
+        </ScrollView>
+    );
+}
---
+++ Exercicios/react-navigation/screens/DrawerScreens/UseMemoScreen.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { ScrollView, Text } from "react-native";
+
+export function UseMemoScreen() {
+    return (
+        <ScrollView>
+            <Text>
+                Retorna um VALOR memoizado.
+
+                Memoização é uma técnica de otimização usada para aumentar o desempenho de 
+                sistemas através de armazenar os resultados de funções “caras” e retornar 
+                o mesmo resultado quando os mesmos parâmetros forem passados novamente. Nesse
+                caso, a memoização evita que seja renderizado várias vezes uma função que
+                retorna sempre o mesmo resultado, evitando cálculos desnecessários.
+
+                O useMemo recebe uma função e um array, e só recupera o valor memoizado 
+                quando o array receber uma atualização. O useMemo é executado durante a 
+                renderização, e não é uma função a ser chamada, mas sim o retorno da função 
+                passada para o useMemo.
+
+                const memoizedValue = useMemo(() => computeExpensiveValue(a, b), [a, b]);
+            </Text>
+        </ScrollView>
+    );
+}
